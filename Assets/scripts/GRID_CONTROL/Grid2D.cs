6f58da173fcaf9bcaaf1a3efcd213f37a0425f4f
raistv <54781272+raistv@users.noreply.github.com>
2021-06-04 17:33:16 +0300

    more grid stuff

---
+++ Assets/scripts/GRID_CONTROL/Grid2D.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  
+/// </summary>
+public class Grid2D
+{
+    private int m_sizeX;
+    private int m_sizeY;
+    Vector2Int m_leftUpCorner;
+    Vector2Int m_rightDownCorner;
+    GridUnitState[,] m_gridStateMatrix;
+
+
+    public  Grid2D(Vector2Int leftUpCorner, Vector2Int rightDownCorner)
+    {
+        m_sizeX = rightDownCorner.x - leftUpCorner.x;
+        m_sizeY = leftUpCorner.y - rightDownCorner.y;
+        if(m_sizeX <= 0|| m_sizeY <= 0)
+        {
+            Debug.LogError("Grid2D, init with bad values check leftUpcorner and rightDownCorner");
+            return;
+        }
+        m_gridStateMatrix = new GridUnitState[m_sizeX, m_sizeY];
+        for(int i=0; i < m_sizeX; i++)
+        {
+            for(int j=0; j<m_sizeY; j++)
+            {
+                m_gridStateMatrix[i,j] = GridUnitState.EMPTY;
+            }
+        }
+    }
+    /// <summary>
+    /// sets gridUnit state success = return true
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="gu"></param>
+    /// <returns></returns>
+    public bool setGridUnitState(int x, int y, GridUnitState gus)
+    {
+        if (!checkValidCordinates(x, y))
+        {
+            return false;
+        }
+        m_gridStateMatrix[x, y] = gus;
+        return true;
+    }
+    public bool checkValidCordinates(int x, int y)
+    {
+        if((x < 0 || x >= m_sizeX) || (y < 0 || y >= m_sizeY))
+        {
+            return false;
+        }
+        return true;
+    }
+}
+public enum GridUnitState
+{
+    EMPTY,
+    OBSTACLE,
+}
---
+++ Assets/scripts/GRID_CONTROL/GridController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public GameObject m_leftUpCornerMarker;
+    public GameObject m_rightDownCornerMarker;
+    private Vector2Int m_leftupVector;
+    private Vector2Int m_rightDownVector;
+    public bool m_debugGrid;
+    public bool m_debugGraph;
+    private Grid2D m_grid2D;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        m_leftupVector = gameObjectoGridVector2Int(m_leftUpCornerMarker);
+        m_rightDownVector = gameObjectoGridVector2Int(m_rightDownCornerMarker);
+        m_grid2D = new Grid2D(m_leftupVector, m_rightDownVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// takes a game object and gives its location grid form [Vector2Int].
+    /// any changes needed to fit grid will happen here.
+    /// </summary>
+    /// <returns></returns>
+    public Vector2Int gameObjectoGridVector2Int(GameObject gameObject)
+    {
+        float x = gameObject.transform.position.x;
+        float y = gameObject.transform.position.y;
+        return new Vector2Int(Mathf.FloorToInt(x),Mathf.FloorToInt(y));
+    }
+    public bool scanEnviormentAndSetGrid()
+    {
+        int x = m_leftupVector.x;
+        int y = m_rightDownVector.y;
+        for(int i = x; i < m_rightDownVector.x; i++)
+        {
+            for(int j = y; j < m_leftupVector.y; j++)
+            {
+                scanSquareEnviorment(new Vector2Int(i, j));
+            }
+        }
+        return true;
+    }
+
+    private GridUnitState scanSquareEnviorment(Vector2Int vector2Int)
+    {
+        throw new NotImplementedException();
+    }
+}
