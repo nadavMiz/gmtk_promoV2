96350d92e2efc5666d44df801864892b13fa4b35
nadav mizrahi <34983648+nadavMiz@users.noreply.github.com>
2021-06-05 17:36:21 +0300

    dfsdfsdfsd

--- Assets/scripts/input/charecterController.cs
+++ Assets/scripts/input/charecterController.cs
@@ -15,6 +15,7 @@ public class charecterController : MonoBehaviour
     static private ulong FULL_GROWTH_SIZE = 10;
     static private ulong GROWTH_RATE = 1;
     static private float ATTACK_MODE_TIME = 10.0f;
+    static private float STUN_TIME = 2f;
 
     public float m_speed = 4.0f;
 
@@ -22,11 +23,15 @@ public class charecterController : MonoBehaviour
     private Collider2D m_collider;
     private Vector3 m_Velocity = Vector3.zero;
     private float m_MovementSmoothing = 0.05f;
-    private int m_stageIdx;
     private CharecterAnimation m_animationController;
 
     private Vector2 m_orientation = Vector2.right;
     private bool m_isAttackMode = false;
+    private bool m_isStuned = false;
+
+    private Vector2 m_moveToDirection = Vector2.zero;
+    private Vector2 m_moveToPosition;
+    private bool m_isMoveTo = false;
 
     [SerializeField] private ulong m_size;
 
@@ -37,23 +42,47 @@ public class charecterController : MonoBehaviour
         m_animationController = GetComponent<CharecterAnimation>();
     }
 
+
+    private void FixedUpdate()
+    {
+        if (m_isMoveTo) 
+        {
+            Move(m_moveToDirection);
+            Vector2 position = new Vector2(transform.position.x, transform.position.y);
+            if (Vector2.Distance(position, m_moveToPosition) < 0.2) 
+            {
+                m_isMoveTo = false;
+            }
+        }
+    }
     public void Move(Vector2 _direction)
     {
+        if (m_isStuned) 
+        {
+            _direction = Vector2.zero;
+        }
         m_animationController.move(_direction);
         changeOrientation(_direction);
         Vector3 targetVelocity = new Vector2(_direction.x * m_speed, _direction.y * m_speed);
         m_rigidbody2D.velocity = Vector3.SmoothDamp(m_rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
     }
 
+    public void MoveTo(Vector2 _target) 
+    {
+        m_moveToPosition = _target;
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        m_moveToDirection = (_target - position).normalized;
+        m_isMoveTo = true;
+    }
+
     public void eat(GameObject obj) 
     {
+        if (m_size == FULL_GROWTH_SIZE) 
+        {
+            return;
+        }
         m_size += GROWTH_RATE;
         Debug.Log("num num " + m_size);
-        while (m_stageIdx < m_stages.Count && m_size >= m_stages[m_stageIdx].m_growthRate) 
-        {
-            ++m_stageIdx;
-            grow();
-        }
         if (m_size >= FULL_GROWTH_SIZE) 
         {
             startAttackMode();
@@ -70,34 +99,42 @@ public class charecterController : MonoBehaviour
 
     public void stun() 
     {
+        m_isStuned = true;
+        m_animationController.stun(true);
+        Invoke("endStunMode", STUN_TIME);
         Debug.Log("I am stuned");
     }
 
     public void getHit() 
     {
+        m_animationController.initiateDeath();
         Debug.Log("I am hit");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        charecterController other = collision.GetComponent<charecterController>();
+        if (other != null) 
+        {
+            checkCollition(other);
+            return;
+        }
         collision.SendMessage("chararcterCollide", m_collider);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void checkCollition(charecterController other)
     {
-        charecterController other = collision.gameObject.GetComponent<charecterController>();
-        if (other == null || m_isAttackMode) 
+        if (m_isAttackMode) 
         {
             return;
         }
-
-        if (other.isAttackMode()) 
+        if (other.isAttackMode())
         {
             getHit();
             return;
         }
 
-        if (other.getLevel() > getLevel()) 
+        if (other.getLevel() > getLevel())
         {
             stun();
         }
@@ -136,7 +173,11 @@ public class charecterController : MonoBehaviour
         }
     }
 
-    private void grow() { /*TODO need animations*/}
+    private void endStunMode() 
+    {
+        m_isStuned = false;
+        m_animationController.stun(false);
+    }
 
     private void endAttackMode() 
     {
