964f3e06bd3326f3da129b12d142ab6c2dbc7e27
nadav mizrahi <34983648+nadavMiz@users.noreply.github.com>
2021-06-05 17:56:27 +0300

    hkjkj

--- Assets/scripts/endGame.cs
+++ Assets/scripts/endGame.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class endGame : MonoBehaviour
 {
-    
+    static public TextMeshPro m_text;
+
+    static string winMessage = "You win!";
+    static string loseMessage = "You Lose!";
     static public void endTheGame(bool isWin) 
     {
-        
+        m_text.SetText(isWin ? winMessage : loseMessage);
     }
 }
